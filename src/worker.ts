0a6f9c137f3519f7391456de95dc2d99955211dd
DerThorsten <derthorstenbeier@gmail.com>
2021-10-27 11:04:41 +0200

    updated to xeus 2.3.0

--- src/worker.ts
+++ src/worker.ts
@@ -52,11 +52,11 @@ async function loadCppModule(moduleFactory: any): Promise<any> {
     raw_xserver = raw_xkernel.get_server();
 
     raw_xserver!.register_js_callback(
-      (type: string, channel: number, data: any) => {
-        data = JSON.parse(data);
+      (type: string, channel: number, message: any) => {
+        console.log("js_callback \n",message)
         switch (type) {
           case 'shell': {
-            postMessageToMain(data, 'shell');
+            postMessageToMain(message, 'shell');
             break;
           }
           case 'control': {
@@ -64,12 +64,12 @@ async function loadCppModule(moduleFactory: any): Promise<any> {
             break;
           }
           case 'stdin': {
-            postMessageToMain(data, 'stdin');
+            postMessageToMain(message, 'stdin');
             break;
           }
           case 'publish': {
             // TODO ask what to do with channel
-            postMessageToMain(data, 'iopub');
+            postMessageToMain(message, 'iopub');
             break;
           }
         }
@@ -97,7 +97,7 @@ ctx.onmessage = async (event: MessageEvent): Promise<void> => {
   if (msg_type === 'input_reply') {
     resolveInputReply(msg.content);
   } else {
-    const str_msg = JSON.stringify(msg);
-    raw_xserver!.notify_listener(str_msg, msg.channel);
+    console.log("JS: notify_listener\n",msg)
+    raw_xserver!.notify_listener(msg);
   }
 };
